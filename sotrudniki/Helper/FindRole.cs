4f6faae3c27f6b3037de33ecda98a362fe9bac9d
unknown <@CHTOTIB.RU>
2024-04-18 09:08:28 +0900

    Добавлены представления данных по моделям

---
+++ sotrudniki/Helper/FindRole.cs
@@ -0,0 +1,22 @@
+﻿using sotrudniki.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sotrudniki.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+}
--- sotrudniki/View/WindowEmployee.xaml.cs
+++ sotrudniki/View/WindowEmployee.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using sotrudniki.Helper;
+using sotrudniki.Model;
+using sotrudniki.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +26,30 @@ namespace sotrudniki.View
         public WindowEmployee()
         {
             InitializeComponent();
+            PersonViewModel vmPerson = new PersonViewModel();
+            RoleViewModel vmRole = new RoleViewModel();
+            List<Role> roles = new List<Role>();
+            foreach (Role r in vmRole.ListRole)
+            {
+                roles.Add(r);
+            }
+            ObservableCollection<PersonDPO> persons = new ObservableCollection<PersonDPO>();
+            FindRole finder;
+            foreach (var p in vmPerson.ListPerson)
+            {
+                finder = new FindRole(p.RoleId);
+                Role rol = roles.Find(new Predicate<Role>(finder.RolePredicate));
+                persons.Add(new PersonDPO
+                {
+                    Id = p.Id,
+                    Role = rol.NameRole,
+                    FirstName = p.FirstName,
+                    LastName = p.LastName,
+                    Birthday = p.Birthday
+                });
+            }
+            lvEmployee.ItemsSource = persons;
         }
     }
 }
+
